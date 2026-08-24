857a2b8e01fa5755785923165280b363a5da6e1d
Grzegorz Bainczyk <grzegorz.bainczyk@gmail.com>
2026-08-24 13:42:28 +0200

    Add first-run welcome guide

--- TTOptimizer.Web/wwwroot/js/main.js
+++ TTOptimizer.Web/wwwroot/js/main.js
@@ -4,14 +4,14 @@ import { initializeOptimization } from "./main/optimization-service.js";
 import { initializeTimetable } from "./main/timetable.js";
 import { initializeCsvExport } from "./main/csv-export.js";
 import { initializeSchoolSummary } from "./main/school-summary.js";
-import { initializeSetupStatus } from "./main/setup-status.js";
+import { initializeWelcomeGuide } from "./main/welcome-guide.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await initializeI18n();
+    initializeWelcomeGuide();
     setupDashboardNavigation();
     initializeTimetable();
     initializeCsvExport();
     await initializeSchoolSummary();
-    await initializeSetupStatus();
     await initializeOptimization();
 });
