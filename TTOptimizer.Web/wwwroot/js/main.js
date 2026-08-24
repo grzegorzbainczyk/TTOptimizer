1d5802cb455283c65222e3ff29f651af711f337b
Grzegorz Bainczyk <grzegorz.bainczyk@gmail.com>
2026-08-24 12:41:42 +0200

    Add school setup status dashboard

--- TTOptimizer.Web/wwwroot/js/main.js
+++ TTOptimizer.Web/wwwroot/js/main.js
@@ -4,6 +4,7 @@ import { initializeOptimization } from "./main/optimization-service.js";
 import { initializeTimetable } from "./main/timetable.js";
 import { initializeCsvExport } from "./main/csv-export.js";
 import { initializeSchoolSummary } from "./main/school-summary.js";
+import { initializeSetupStatus } from "./main/setup-status.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await initializeI18n();
@@ -11,5 +12,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     initializeTimetable();
     initializeCsvExport();
     await initializeSchoolSummary();
+    await initializeSetupStatus();
     await initializeOptimization();
 });
